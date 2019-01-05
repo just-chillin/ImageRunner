e3f55e2c9714354132005f751686279509eb6ed6
Devin <eflat247@gmail.com>
2019-01-04 22:47:51 -0500

    CLI Ver 3.0

--- Main.py
+++ Main.py
@@ -5,6 +5,10 @@ def main():
         path, lang = input("Please enter the file path and the programming language separated by a comma \n").split(",")
         print(path, lang)
         #OCR_main()
+
+
+
+
         print('Would you like to input another file? (Yes, Y, y, or No, N, n)')
         x = input()
         if(x == 'No' or x == 'N' or x == 'n'):
