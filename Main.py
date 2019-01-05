34d9a8219c0838d500c7352f0dce71f884860f43
Devin <eflat247@gmail.com>
2019-01-04 22:31:52 -0500

    CLI Ver 2.0

--- Main.py
+++ Main.py
@@ -2,10 +2,6 @@ def main():
     print("Welcome to ImageRunner")
     Loop_var = True
     while Loop_var:
-        """print("Please enter the file path ")
-        Path_var = input()
-        print("Please enter the programming language ")
-        Lang_var = input()"""
         path, lang = input("Please enter the file path and the programming language separated by a comma \n").split(",")
         print(path, lang)
         #This is where the program runs
