8fc3819e79d5ea3d949c3aa4eab0c7e74b2aa22d
Devin <eflat247@gmail.com>
2019-01-04 21:58:58 -0500

    CLI Ver 2.0

--- Main.py
+++ Main.py
@@ -1,12 +1,17 @@
-print("Welcome to ImageRunner")
-Loop_var = True
-while Loop_var:
-    print("Please enter the file path ")
-    Path_var = input()
-    print("Please enter the programming language ")
-    Lang_var = input()
-    #This is where the program runs
-    print('Would you like to input another file? (Yes, Y, y, or No, N, n)')
-    x = input()
-    if(x == 'No' or x == 'N' or x == 'n'):
-        Loop_var = False
+def main():
+    print("Welcome to ImageRunner")
+    Loop_var = True
+    while Loop_var:
+        print("Please enter the file path ")
+        Path_var = input()
+        print("Please enter the programming language ")
+        Lang_var = input()
+        #This is where the program runs
+        print('Would you like to input another file? (Yes, Y, y, or No, N, n)')
+        x = input()
+        if(x == 'No' or x == 'N' or x == 'n'):
+            Loop_var = False
+
+
+if __name__ == "__main__":
+    main()
