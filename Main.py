af9471af2738afb3f209d238172d47ac3000de55
Devin <eflat247@gmail.com>
2019-01-05 00:55:19 -0500

    CLI Ver 4.0

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 import json
 def main():
     print("Welcome to ImageRunner")
-    Loop_var = True
+    Loop = True
     First_prompt = True
-    while Loop_var:
+    while Loop:
+        Incor = True
         while First_prompt:
             #loop runs at least once and requests 2 inputs from the user
             result = input("Please enter the file path and the programming language separated by a comma\n").split(",")
@@ -20,13 +21,15 @@ def main():
         #OCR_main()
 
 
-
-        #prompts the user to run the program again or exits
-        x = input('Would you like to input another file? (Yes, yes, Y, y, or No, no, N, n)\n')
-        if (x == 'Yes' or x == 'Y' or x == 'y' or x == 'yes'):
-            First_prompt = True
-        if(x == 'No' or x == 'N' or x == 'n' or x == 'no'):
-            Loop_var = False
+        while(Incor):
+            #prompts the user to run the program again or exits
+            x = input('Would you like to input another file? (Yes, yes, Y, y, or No, no, N, n)\n')
+            if (x == 'Yes' or x == 'Y' or x == 'y' or x == 'yes'):
+                First_prompt = True
+                Incor = False
+            if(x == 'No' or x == 'N' or x == 'n' or x == 'no'):
+                Loop = False
+                Incor = False
 
     print("You've reached the end of the program!")
 
