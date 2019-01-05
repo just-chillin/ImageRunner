acf33c6c1370fbd3ac1024cf1e3dc8917c22a3b7
Colin Sivak <abounding05@gmail.com>
2019-01-04 22:48:11 -0500

    Created some basic request code

--- OCR.py
+++ OCR.py
@@ -1,2 +1,33 @@
 from google.cloud import vision
+import requests
 
+
+
+def ocr_main(file, lang):
+    headers = {
+        "Authorization": "Bearer AIzaSyBvmh8i8Xenxa64k-DIo27nF6zykGK1zCU",
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    data = {
+        'requests': [
+            {
+                'image': {
+                    'source': {
+                        'imageUri': file
+                    }
+                },
+                'features': [
+                    {
+                        'type': 'TEXT_DETECTION'
+                    }
+                ]
+            }
+        ]
+    }
+    req = requests.post(url='https://vision.googleapis.com/v1/images:annotate', headers=headers, json=data)
+    print(req.reason)
+
+
+
+if __name__ == "__main__":
+    ocr_main('text.pdf', 'python')
