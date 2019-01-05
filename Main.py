14cb490ad79161db80be973fae76799ab5d11127
Colin Sivak <abounding05@gmail.com>
2019-01-05 11:19:37 -0500

    Created code to read extension from config file

--- Main.py
+++ Main.py
@@ -1,16 +1,22 @@
 import json
+from OCR import ocr_main
 
 
 def prompt_user():
     # Prompt the user for a language
     print("Welcome to ImageRunner")
-    path = input("Please enter the path to the image: ")
-    language = input("Please enter the programming language: ")
+    path = 'js_code.png'
+    language = 'js'
+    # path = input("Please enter the path to the image: ")
+    # language = input("Please enter the programming language: ")
     return path, language
 
 
 def main():
-    path, language = prompt_user()
+    image, language = prompt_user()
+    settings = json.load(open('config.json'))[language]
+    ocr_main(image, settings["extension"])
+
 
 
 if __name__ == '__main__':
